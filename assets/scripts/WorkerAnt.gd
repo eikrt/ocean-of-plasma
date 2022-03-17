a4aee01cdae217ddd24a17198f4fe224f87196f6
Eino Korte <e.i.korte@gmail.com>
2022-03-17 19:33:57 +0200

    added movement

--- assets/scripts/WorkerAnt.gd
+++ assets/scripts/WorkerAnt.gd
@@ -1,37 +1,46 @@
 extends "Ant.gd"
-const SPEED = 10
+const SPEED = 12
+const TARGET_RANGE = 400
 var walk_change = 0
-var walk_time = 5
+var walk_time = 10
 var input_vector = Vector3()
 var target = Vector3()
+var targetObject
+var targetTransform
 var hasTurnip = false
 var Turnip = preload("res://assets/scripts/Turnip.gd")
 var Storage = preload("res://assets/scripts/Vase.gd")
 var storage
+var g= Vector3()
 func _physics_process(delta):
 	._physics_process(delta)
 	if hasTurnip:
 		$Turnip.visible = true
 		current_action = "walk_towards"
-		target.x = storage.transform.origin.x
-		target.y = storage.transform.origin.y
-		target.z = storage.transform.origin.z
+		target.x = storage.global_transform.origin.x
+		target.y = storage.global_transform.origin.y
+		target.z = storage.global_transform.origin.z
 	else:
 		$Turnip.visible = false
+
 	if current_action == "find":
-		target.x = self.translation.x + rand_range(-100,0) + 50
-		target.z = self.translation.z + rand_range(-100,0) + 50
+		randomize()
+		target.x = self.global_transform.origin.x + rand_range(-TARGET_RANGE,0) + TARGET_RANGE/2
+		target.z = self.global_transform.origin.z + rand_range(-TARGET_RANGE,0) + TARGET_RANGE/2
+		target.y = self.global_transform.origin.y
+		#print(target.x)
 		current_action = "walk"
 	if current_action == "walk":
 		walk_change += delta
 		if walk_change > walk_time:
+			walk_time = rand_range(5,10)
 			walk_change = 0
 			current_action = "find"
 	if current_action == "walk_towards" or current_action == "walk":
-		#print(self.translation, " ", target)
-		var flat_translation = Vector3(self.translation.x, self.translation.y, self.translation.z)
-		#flat_translation.y = target.y
-		translation = self.translation.move_toward(target, delta * SPEED)
+		var local_target = self.global_transform.xform_inv(target)
+		self.translation = self.translation.move_toward(local_target, SPEED * delta)
+		
+
 		
 
 
@@ -50,14 +59,17 @@ func _on_TurnipVicinity_area_entered(area):
 		if current_action == "walk":
 
 			if area is Turnip and is_instance_valid(area):
-				current_action = "walk_towards"
-				target.x = area.global_transform.origin.x
-				target.y = area.global_transform.origin.y
-				target.z = area.global_transform.origin.z
+				#current_action = "walk_towards"
+				#targetObject = area
 				#target.x = area.translation.x
 				#target.z = area.translation.z
 				#target.y = area.translation.y
-				
+				#target.x = area.global_transform.origin.x
+				#target.y = area.global_transform.origin.y
+				#target.z = area.global_transform.origin.z
+				#print("dsf")
+				#targetTransform = area.global_transform
+				pass
 
 func _on_Vicinity_area_entered(area):
 	if area.name == "Turnip":
